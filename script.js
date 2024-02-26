0bd029b9c8703358ae2ea03f2e012d41586dd5a7
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-02-26 20:22:02 +0530

    this method for add transaction

--- script.js
+++ script.js
@@ -6,13 +6,40 @@ const form = document.getElementById('form');
 const amount = document.getElementById('amount');
 
 const dummyTransactions = [
-    {id:1 , text:'Flower',amount:-20},
-    {id:2, text:'salary' , amount:300},
-    { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 }
+   { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 300 },
+   { id: 3, text: 'Book', amount: -10 },
+   { id: 4, text: 'Camera', amount: 150 }
 ];
 
 let transactions = dummyTransactions;
+
+
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a text and amount');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value
+    };
+
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    updateLocalStorage();
+
+    text.value = '';
+    amount.value = '';
+  }
+}
 
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
@@ -23,17 +50,40 @@ function addTransactionDOM(transaction) {
   
     // Add class based on value
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
+
+  item.innerHTML = `
+    ${transaction.text} <span>${sign}${Math.abs(
+    transaction.amount
+  )}</span> <button class="delete-btn" onclick="removeTransaction(${
+    transaction.id
+  })">x</button>
+  `;
+
+  list.appendChild(item);
+}
+
+// update the balance , income, and expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  ).toFixed(2);
+
   
-    item.innerHTML = `
-      ${transaction.text} <span>${sign}${Math.abs(
-      transaction.amount
-    )}</span> <button class="delete-btn" onclick="removeTransaction(${
-      transaction.id
-    })">x</button>
-    `;
-  
-    list.appendChild(item);
-  }
+  balance.innerText = `$${total}`;
+  money_plus.innerText = `$${income}`;
+  money_minus.innerText = `$${expense}`;
+}
+
 
 
   // Init app
@@ -46,4 +96,5 @@ function init() {
   
   init();
   
+  form.addEventListener('submit', addTransaction);
 
